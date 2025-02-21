1bfb21e1f2f2eaa9755f82d85d782e09361cf817
KyoAzami <kyotaro.azami@gmail.com>
2025-02-21 00:47:25 -0600

    Cambios para mejorar la funcionalida de lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,24 +18,28 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls", "clangd", "cssls", "html"}
+                ensure_installed = {"lua_ls", "clangd", "cssls", "html", "bashls", "pyright", "jdtls", "rust_analyzer", "gopls", "texlab"},
+                automatic_installation = true,  -- Esto asegura que los servidores se instalen automáticamente
             })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require('lspconfig')
 
-            local servers = { "lua_ls", "bashls", "clangd", "html", "cssls", "tsserver", "pyright", "jdtls", "rust_analyzer", "gopls", "texlab" }
+            -- Esto asegura que los servidores LSP se configuren al abrir archivos del tipo adecuado
+            local lspconfig = require('lspconfig')
+            local servers = { "lua_ls", "bashls", "clangd", "html", "cssls", "pyright", "jdtls", "rust_analyzer", "gopls", "texlab" }
 
             for _, server in ipairs(servers) do
+                -- Llama al servidor LSP correspondiente y configura sus capacidades
                 lspconfig[server].setup({
                     capabilities = require('cmp_nvim_lsp').default_capabilities(),
+                    on_attach = function(client, bufnr)
+                    end,
                 })
             end
         end
+    },
+    {
+        "neovim/nvim-lspconfig",  --la integración con mason-lspconfig se puede hacer aquí
+        config = function()
+        end
     }
-
 }
 
