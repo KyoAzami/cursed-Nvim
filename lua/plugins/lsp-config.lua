840c089edd96372f37dd0aec3df56e05ddab4585
KyoAzami <kyotaro.azami@gmail.com>
2025-02-26 21:20:13 -0600

    cambio definitivo a lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,82 +8,33 @@
 
 
 return {
-    -- Autocompletado principal
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",  -- Integración con LSP
-            "hrsh7th/cmp-buffer",    -- Autocompletado desde el buffer
-            "hrsh7th/cmp-path",      -- Autocompletado de rutas
-            "hrsh7th/cmp-cmdline",   -- Autocompletado en la línea de comandos
-        },
-        config = function()
-            local cmp = require("cmp")
-            
-            -- Configuración de nvim-cmp
-            cmp.setup({
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },      -- Fuente para sugerencias LSP
-                    { name = "buffer" },        -- Fuente para sugerencias desde el buffer actual
-                    { name = "path" },          -- Fuente para autocompletado de rutas de archivos
-                }),
-                mapping = cmp.mapping.preset.insert({
-                    ["<Tab>"] = cmp.mapping.select_next_item(),  -- Seleccionar siguiente ítem
-                    ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- Seleccionar ítem anterior
-                    ["<C-Space>"] = cmp.mapping.complete(),  -- Invocar el autocompletado
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),  -- Confirmar selección
-                }),
-            })
-        end,
-    },
-
-    -- Configuración de servidores LSP
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp"
+        },
         config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()  -- Obtener capacidades para autocompletado
+            local lspconfig = require'lspconfig'
+            local cmp = require'cmp'
+            local cmp_nvim_lsp = require'cmp_nvim_lsp'
 
-            local on_attach = function(client, bufnr)
-                local opts = { noremap = true, silent = true, buffer = bufnr }
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)  -- Mostrar información de símbolo
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)  -- Ir a la definición
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)  -- Ir a la implementación
-                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)  -- Renombrar variable/símbolo
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+
+            -- Servidores LSP para diferentes lenguajes
+            local servers = {
+                'html', 'cssls', 'tsserver', 'eslint', 'jsonls', 'vuels', 'svelte', 'tailwindcss', -- Desarrollo web
+                'gopls', 'rust_analyzer', 'jdtls', 'clangd', 'csharp_ls', 'asm_lsp', 'cobol_ls', -- Lenguajes específicos
+                'phpactor', 'pyright', 'lua_ls', 'bashls' -- Otros lenguajes comunes
+            }
+
+            for _, server in ipairs(servers) do
+                lspconfig[server].setup {
+                    capabilities = capabilities
+                }
             end
-
-            -- Configuración automática de servidores LSP instalados con Mason
-            require("mason-lspconfig").setup_handlers({
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        capabilities = capabilities,
-                        on_attach = on_attach,
-                    })
-                end,
-            })
-        end,
-    },
-
-    -- Instalador de servidores LSP
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-
-    -- Integración con mason-lspconfig para configurar automáticamente los servidores LSP
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls", "clangd", "cssls", "html", "bashls", "pyright", 
-                    "jdtls", "rust_analyzer", "gopls", "texlab", "emmet_ls"
-                },
-                automatic_installation = true,  -- Instalar automáticamente los servidores que no están instalados
-            })
-        end,
-    },
+        end
+    }
 }
+
 
