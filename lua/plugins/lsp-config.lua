c2cb05b19fb24efd58ef2f31e6a68d13bd5cab40
KyoAzami <kyotaro.azami@gmail.com>
2025-02-26 21:40:26 -0600

    ayuda no quiero configurar snips

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,10 +7,18 @@
                                                                              
 
 
+
 return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
     {
         "neovim/nvim-lspconfig",
         dependencies = {
+            "williamboman/mason-lspconfig.nvim",
             "hrsh7th/nvim-cmp",
             "hrsh7th/cmp-nvim-lsp"
         },
@@ -18,16 +26,24 @@ return {
             local lspconfig = require'lspconfig'
             local cmp = require'cmp'
             local cmp_nvim_lsp = require'cmp_nvim_lsp'
+            local mason_lspconfig = require'mason-lspconfig'
 
             local capabilities = cmp_nvim_lsp.default_capabilities()
+            capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-            -- Servidores LSP para diferentes lenguajes
+            -- Lista de servidores LSP
             local servers = {
                 'html', 'cssls', 'eslint', 'jsonls', 'vuels', 'svelte', 'tailwindcss', -- Desarrollo web
                 'gopls', 'rust_analyzer', 'jdtls', 'clangd', 'csharp_ls', 'asm_lsp', 'cobol_ls', -- Lenguajes específicos
                 'phpactor', 'pyright', 'lua_ls', 'bashls' -- Otros lenguajes comunes
             }
 
+            -- Instalar servidores LSP automáticamente con Mason
+            mason_lspconfig.setup {
+                ensure_installed = servers
+            }
+
+            -- Configuración de cada servidor LSP
             for _, server in ipairs(servers) do
                 lspconfig[server].setup {
                     capabilities = capabilities
