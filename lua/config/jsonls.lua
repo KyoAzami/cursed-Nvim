35562693ec668bc5daa6641d1b56c450c374ecb0
KyoAzami <kyotaro.azami@gmail.com>
2025-05-17 00:15:00 -0600

    creo que ya quedo

---
+++ lua/config/jsonls.lua
@@ -0,0 +1,20 @@
+--     ██ ███████  ██████  ███    ██ ██      ███████ 
+--     ██ ██      ██    ██ ████   ██ ██      ██      
+--     ██ ███████ ██    ██ ██ ██  ██ ██      ███████ 
+--██   ██      ██ ██    ██ ██  ██ ██ ██           ██ 
+-- █████  ███████  ██████  ██   ████ ███████ ███████ 
+                                                   
+                                                   
+
+local lspconfig = require("lspconfig")
+local schemastore = require("schemastore")
+
+lspconfig.jsonls.setup({
+  settings = {
+    json = {
+      schemas = schemastore.json.schemas(),
+      validate = { enable = true },
+    },
+  },
+})
+
